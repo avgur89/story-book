59cbc1f5285ef28176c4e664aad121db4886015a
alexgurov <avgur89@gmail.com>
2020-06-23 16:23:52 +0300

    Add google passport strategy

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
+const passport = require('passport');
+const session = require('express-session');
 const connectDB = require('./config/db');
+
+const mainRoutes = require('./routes');
+const authRoutes = require('./routes/auth');
 
 // Load config
 dotenv.config({ path: './config/config.env' });
+
+// Passport config
+require('./config/passport')(passport);
 
 connectDB();
 
@@ -19,6 +28,26 @@ if (process.env.NODE_ENV === 'development') {
 // Handlebars
 app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', '.hbs');
+
+// Sessions
+app.use(
+  session({
+    secret: 'secret word',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/', mainRoutes);
+app.use('/auth', authRoutes);
 
 const PORT = process.env.PORT || 3000;
 
