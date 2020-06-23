c1adc8f313d6277176b417c305dfe00c0c0ffb36
alexgurov <avgur89@gmail.com>
2020-06-23 17:19:58 +0300

    Create routes and template for a new story

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const path = require('path');
 const express = require('express');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
 const passport = require('passport');
 const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 const connectDB = require('./config/db');
 
 const mainRoutes = require('./routes');
 const authRoutes = require('./routes/auth');
+const storiesRoutes = require('./routes/stories');
 
 // Load config
 dotenv.config({ path: './config/config.env' });
@@ -19,6 +22,10 @@ require('./config/passport')(passport);
 connectDB();
 
 const app = express();
+
+// Body parser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // Logging
 if (process.env.NODE_ENV === 'development') {
@@ -35,6 +42,7 @@ app.use(
     secret: 'secret word',
     resave: false,
     saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
   })
 );
 
@@ -48,6 +56,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Routes
 app.use('/', mainRoutes);
 app.use('/auth', authRoutes);
+app.use('/stories', storiesRoutes);
 
 const PORT = process.env.PORT || 3000;
 
